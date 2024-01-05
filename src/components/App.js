8091d01d0fadc95f572215b053794502f0777e2c
Milan <m26030820@gmail.com>
2024-01-05 16:09:11 +0100

    adding react props and using react devtool

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import Card from './Card';
 import contacts from '../contact';
+import Avatar from './Avatar';
 
-// Hello 
 
 function App() {
   return ( 
       <div>
         <h1 className="heading">My Contacts</h1>
+        <Avatar img="https://media.licdn.com/dms/image/D4D03AQEbogRbTpPBpg/profile-displayphoto-shrink_200_200/0/1694187086111?e=2147483647&v=beta&t=WBIzvYD2PESBTLSywJjjMv29eBL_MNUNrtdJYTyOVzM" />
         <Card
           name={contacts[0].name}
           img= {contacts[0].imgURL}
