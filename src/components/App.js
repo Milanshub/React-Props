f8a5b407307fcf3ac4ca8e8e8a6d9c281065f323
Milan <m26030820@gmail.com>
2024-01-07 10:21:17 +0100

    learning mapping data to components

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,27 @@ import Card from "./Card";
 import contacts from '../contact';
 import Avatar from "./Avatar";
 
+
+//create a mapping date that loops through js objects in contacts 
+function createCard(contact){
+  return <Card 
+  key={contact.id}
+  name={contact.name} 
+  img={contact.imgURL}
+  tel={contact.phone}
+  email={contact.email} />
+}
+
+
 function App() {
   return ( 
       <div>
         <h1 className="heading">My Contacts</h1>
         <Avatar img='https://media.licdn.com/dms/image/D4D03AQEbogRbTpPBpg/profile-displayphoto-shrink_200_200/0/1694187086111?e=2147483647&v=beta&t=WBIzvYD2PESBTLSywJjjMv29eBL_MNUNrtdJYTyOVzM' />
-        <Card
+
+        {contacts.map(createCard)}
+
+        {/* <Card
           name={contacts[0].name}
           img= {contacts[0].imgURL}
           tel={contacts[0].phone}
@@ -21,7 +36,7 @@ function App() {
           name={contacts[2].name}
           img= {contacts[2].imgURL}
           tel={contacts[2].phone}
-          email={contacts[2].email} />
+          email={contacts[2].email} /> */}
       </div>
   );
 }
